99cf4e34de6d39674b875db83cc8bafa8d6fca61
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-06-11 21:29:34 -0300

    Add files via upload

---
+++ scripr.js
@@ -0,0 +1,14 @@
+//alert('js carregado');
+
+var btnMenu = document.querySelector(".btnMenu");
+var menu = document.querySelector(".menu");
+
+btnMenu.addEventListener("click", function () {
+  menu.classList.toggle("menu-open");
+  btnMenu.classList.toggle('x');
+});
+
+menu.addEventListener("click", function () {
+  menu.classList.remove(menu-open);
+  menu.classList.remove("x");
+});
