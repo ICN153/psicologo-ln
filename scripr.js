e0e98b1a7f0ef410b02ebd77de6d3fe4d28a8305
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-06-17 22:37:12 -0300

    Add files via upload

--- scripr.js
+++ scripr.js
@@ -5,10 +5,28 @@ var menu = document.querySelector(".menu");
 
 btnMenu.addEventListener("click", function () {
   menu.classList.toggle("menu-open");
-  btnMenu.classList.toggle('x');
+  btnMenu.classList.toggle("x");
 });
 
 menu.addEventListener("click", function () {
-  menu.classList.remove(menu-open);
+  menu.classList.remove(menu - open);
   menu.classList.remove("x");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+      let message = document.getElementById("message").value;
+
+      let whatsappMessage = `Nome: ${name}%0AEmail: ${email}%0ATelefone: ${phone}%0AMensagem: ${message}`;
+      let whatsappUrl = `https://5521974866272?text=${whatsappMessage}`;
+
+      window.open(whatsappUrl, "_blank");
+    });
 });
